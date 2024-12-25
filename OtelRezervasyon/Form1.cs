6d7ee058dd639c16c66e83da1ffc9cac513955ca
Murat ÇIPLAK <50632672+muratapk@users.noreply.github.com>
2024-12-25 21:32:32 +0300

    Add project files.

---
+++ OtelRezervasyon/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtelRezervasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HotelDbEntities conn=new HotelDbEntities();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Admin admin = new Admin();
+            admin.UserName = textBox1.Text;
+            admin.UserPassword = textBox2.Text;
+            admin.Authority = comboBox1.Text;
+            conn.Admin.Add(admin);  
+            conn.SaveChanges();
+            MessageBox.Show("Kayıt Edildi");
+            doldur();
+
+        }
+        void doldur()
+        {
+            var tablom = conn.Admin.ToList();
+            dataGridView1.DataSource = tablom;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int satir = dataGridView1.CurrentRow.Index;
+            //satir numarasını al
+            textBox1.Text = dataGridView1.Rows[satir].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[satir].Cells[2].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[satir].Cells[3].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
+            //id numarasını textbox3.text 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt16(textBox3.Text);
+            var tablom = conn.Admin.Where(x => x.UserId == Id).FirstOrDefault();
+            conn.Admin.Remove(tablom);
+            conn.SaveChanges();
+            MessageBox.Show("Kayıt Silindi");
+            doldur();
+        }
+    }
+}
